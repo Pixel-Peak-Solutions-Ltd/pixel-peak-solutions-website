34189ca01ea2659e7ba15a510eec20275f64d545
Saif64 <saif.vivasoft@bkash.com>
2025-08-24 16:04:16 +0600

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: 'export', // This is still needed for Render
+  // basePath and assetPrefix have been removed
   webpack(config) {
     // Grab the existing rule that handles SVG imports
     const fileLoaderRule = config.module.rules.find((rule) =>
